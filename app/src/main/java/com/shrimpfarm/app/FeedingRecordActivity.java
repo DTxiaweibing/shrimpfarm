ded6b37f64be8523b4c0b6e8649a3af93b6cb309
DTxiaweibing <1032699170@QQ.com>
2026-09-09 00:07:41 +0800

    fix: 记录表产品单元格恢复自动换行(两行); 版本号升至1.5.5

--- app/src/main/java/com/shrimpfarm/app/FeedingRecordActivity.java
+++ app/src/main/java/com/shrimpfarm/app/FeedingRecordActivity.java
@@ -711,7 +711,8 @@ public class FeedingRecordActivity extends BaseActivity {
             et.setBackground(createCellBorder(0xFFFFFFFF));
             et.setPadding(0, 0, 0, 0);
             et.setOverScrollMode(View.OVER_SCROLL_NEVER);
-            et.setSingleLine(true);
+            et.setSingleLine(false);
+            et.setMaxLines(2);
             et.setFocusable(false);
             et.setFocusableInTouchMode(false);
             et.setClickable(true);
